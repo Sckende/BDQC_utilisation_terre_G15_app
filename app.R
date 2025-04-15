fe468a490e0a3878e50bb66798486d21f3a72a4c
Sckende <clrccljuhasz@gmail.com>
2025-04-15 11:39:50 -0400

    ajout comparaison donnees ESA-ISQ

--- app.R
+++ app.R
@@ -17,10 +17,19 @@ library(plotly)
 library(ggplot2)
 library(terra)
 
+options(shiny.launch.browser = TRUE)
 #### Local data ####
 # ---------------- #
 # utilisation des terres
 comp <- readRDS("/home/local/USHERBROOKE/juhc3201/BdQc/ReseauSuivi/Data/g15_indicators/results/ESA/2010-2020_comp2010_ISQ_reg.rds")
+
+# donnees prop habitats naturels ISQ
+isq <- read.csv("/home/local/USHERBROOKE/juhc3201/BdQc/ReseauSuivi/Data/g15_indicators/ISQ/QC_meridional_natural_land_prop_2010.csv", h = T, encoding = "latin1")
+isq_nat <- isq[isq$year == 2010, ]
+isq_nat$region[isq_nat$region == "Gaspésie-Îles-de-la-Madeleine"] <- "Gaspésie–Îles-de-la-Madeleine"
+# donnees prop habitats naturels ESA
+esa_nat <- readRDS("/home/local/USHERBROOKE/juhc3201/BdQc/ReseauSuivi/Data/g15_indicators/results/ESA/ESA_prop_hab_nat_2010.rds")
+
 
 # Several Polygons for Qc
 # -----------------------
@@ -67,7 +76,7 @@ server <- function(input, output, session) {
             geom_line(linewidth = 1) +
             labs(title = unique(x$region), color = "Catégories") +
             scale_color_manual(
-                labels = c("agriculture", "forest", "grassland", "other", "settlement", "wetland"),
+                labels = c("agriculture", "forêt", "prairie", "autres", "habitation", "milieu humide"),
                 values = c(
                     "#993300",
                     "#006600",
@@ -77,9 +86,6 @@ server <- function(input, output, session) {
                     "#019191"
                 )
             )
-        # +
-        # scale_x_continuous(name = "Année", limits = c(2010, 2020), breaks = 2010:2020) +
-        # scale_y_continuous(name = "Variation (%)", limits = c(-35, 35))
     })
 
     # pie chart
@@ -95,7 +101,11 @@ server <- function(input, output, session) {
         )
 
         fig <- plot_ly(data,
-            labels = ~IPCC_class, values = ~count_tot, type = "pie", textposition = "inside",
+            labels = c("agriculture", "forêt", "prairie", "autres", "habitation", "milieu humide"),
+            # labels = ~IPCC_class,
+            values = ~count_tot,
+            type = "pie",
+            textposition = "inside",
             textinfo = "label+percent",
             insidetextfont = list(color = "#FFFFFF"),
             hoverinfo = "text",
@@ -115,6 +125,68 @@ server <- function(input, output, session) {
             yaxis = list(showgrid = FALSE, zeroline = FALSE, showticklabels = FALSE)
         )
     })
+
+    # comparaison prop habitats naturels ESA vs. ISQ
+    output$hab_nat <- renderPlotly({
+        reg <- input$admin_select
+
+        # data formatting
+        esa <- esa_nat[esa_nat$region == reg, ]
+        esa$data <- "esa"
+        names(esa)[1] <- "nat_hab"
+
+        isq <- isq_nat[isq_nat$region == reg, ]
+        isq$prop <- isq$prop * 100
+        isq$data <- "isq"
+        isq$region <- reg
+
+        cp <- rbind(isq, esa[, c("region", "nat_hab", "prop", "year", "data")])
+        cp <- cp |> mutate(text = sprintf("Données: %s<br> Proportion: %s <br>", data, round(prop, digits = 2)))
+        cp <- cp[order(cp$data, cp$nat_hab), ]
+
+        # figure
+        fig <- cp %>% plot_ly()
+
+        fig <- fig %>% add_trace(
+            x = cp$nat_hab[cp$data == "esa"],
+            y = cp$prop[cp$data == "esa"],
+            type = "bar",
+            text = cp$text[cp$data == "esa"],
+            textposition = "auto",
+            marker = list(
+                color = c("rgba(46,72,62,0.8)", "rgba(123,181,177, 0.8)", "rgba(166,97,45, 0.8)"),
+                line = list(
+                    color = c("rgba(46,72,62,1)", "rgba(123,181,177, 1)", "rgba(166,97,45, 1)"),
+                    width = 1.5
+                )
+            )
+        )
+
+        fig <- fig %>% add_trace(
+            x = cp$nat_hab[cp$data == "isq"],
+            y = cp$prop[cp$data == "isq"],
+            type = "bar",
+            text = cp$text[cp$data == "isq"],
+            textposition = "auto",
+            marker = list(
+                color = c("rgba(46,72,62,0.5)", "rgba(123,181,177, 0.5)", "rgba(166,97,45, 0.5)"),
+                line = list(
+                    color = c("rgba(46,72,62,1)", "rgba(123,181,177, 1)", "rgba(166,97,45, 1)"),
+                    width = 1.5
+                )
+            )
+        )
+
+        fig <- fig %>%
+            layout(
+                # title = "Comparaison proportion milieux naturels ESA - ISQ",
+                barmode = "group",
+                xaxis = list(title = ""),
+                yaxis = list(title = ""),
+                showlegend = FALSE
+            ) |>
+            style(hoverinfo = "none")
+    })
 }
 
 ui <- navbarPage(
@@ -122,6 +194,7 @@ ui <- navbarPage(
     sidebarLayout(
         sidebarPanel(
             h4("Région administrative"),
+            width = 2,
             selectInput(
                 inputId = "admin_select",
                 label = "",
@@ -133,17 +206,27 @@ ui <- navbarPage(
         ),
         mainPanel(
             # First row
-            box(
-                title = "Trend",
-                width = 6,
-                plotOutput("trend_plot")
+            fluidRow(
+                box(
+                    title = "Évolution utilisation des terres ESA",
+                    width = 6,
+                    plotOutput("trend_plot")
+                ),
+                box(
+                    title = "Proportion de type de terre - ESA 2010",
+                    width = 6,
+                    plotlyOutput("pie")
+                )
             ),
-            box(
-                title = "Pie chart",
-                width = 6,
-                plotlyOutput("pie")
+            fluidRow(
+                box(
+                    title = "Proportion des milieux naturels ESA - ISQ (2010)",
+                    width = 6,
+                    plotlyOutput("hab_nat")
+                )
             )
         )
     )
 )
-shinyApp(ui = ui, server = server)
+
+shinyApp(ui = ui, server = server, options = list(launch.browser = TRUE))
